f4563df20aa72ef121c547d87cdd12e7b5c05051
Fredrik Román <roman.fredrik@gmail.com>
2013-11-11 19:14:10 +0100

    Changed the behaviour to deper. Now deper takes an array with argument names and the function with the injected dependencies. deper falls back to require if the dependency is not provided

--- test/deper.js
+++ test/deper.js
@@ -3,33 +3,34 @@ var deper = require("../lib/deper.js");
 
 describe("deper", function () {
   it("returns a function that sets the given functions arguments", function () {
-    var func = function (a, b) {
-      return a + b;
+    var func = function (c, d) {
+      return c + d;
     };
-    var res = deper(func)(["a", "b"], "hello ", "world");
+    var res = deper(["a", "b"], func)(["a", "b"], "hello ", "world");
 
     expect(res).to.equal("hello world");
   });
 
-  it("throws an exception if the argument isn't a function", function () {
-    expect(deper.bind(null, "hello")).to.throw();
-  });
-
-  it("throws an exception if the first argument to the returned function isn't an array", function () {
-    var func = function () {};
-    expect(deper(func).bind(null, "hello")).to.throw();
-  });
-
-  it("throws an exception if the number of tail arguments of the returned function isn't the same length as the given array",
-  function () {
-    var func = function () {
+  it("defaults to require if a dependency isn't injected", function () {
+    var res;
+    var func = function (retrieveArguments) {
+      res = retrieveArguments(function (hello, world) {});
     };
-    expect(deper(func).bind(null, ["hello", "world"], "hello", "world", "!")).to.throw();
+    deper(["../lib/argumentRetriever"], func)();
+    expect(res).to.deep.equal(["hello", "world"]);
   });
 
-  it("throws an exception if the given dependecies doesn't match the required dependecies", function () {
+  it("throws an exception if a dependency name array isn't provided", function () {
+    expect(deper.bind(null)).to.throw();
+  });
+
+  it("throws an exception if the argument isn't a function", function () {
+    expect(deper.bind(null, [], "hello")).to.throw();
+  });
+
+  it("throws an exception if the given dependecies doesn't match the required dependencies", function () {
     var func = function (hello, world) {
     };
-    expect(deper(func).bind(null, ["helo", "word"], "hello", "world")).to.throw();
+    expect(deper([], func).bind(null, ["helo", "word"], "hello", "world")).to.throw();
   });
 });
