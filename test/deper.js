f89288108c26dc9f065dbb2be728aa4a2beefbe0
Fredrik Román <roman.fredrik@gmail.com>
2013-11-25 18:20:13 +0100

    Setting defaults in config

--- test/deper.js
+++ test/deper.js
@@ -40,4 +40,24 @@ describe("deper", function () {
     var func = function (hello, world) {};
     expect(deper([], func).bind(null, ["helo", "word"], "hello", "world")).to.throw();
   });
+
+  it("can set default dependencies in the config", function () {
+    var deper = require("../lib/deper.js");
+    deper.config({
+      basePath: __dirname + "/",
+      defaults: {
+        "_": "testModule.js"
+      }
+    });
+
+    var res;
+
+    var func = function (_) {
+      res = _;
+    };
+
+    deper(["_"], func)();
+
+    expect(res).to.equal("hello world");
+  });
 });
